831231ef2d43b1b0ed151b78e31c96b49bd7b5d3
Rithikadevi <rithikadevip2@gmail.com>
2021-04-14 22:39:45 +0530

    my edit

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[]args){
+        int i,a=10 ;
+        float b= 10.2f;
+        char c='c';
+        String d="i'm Rithika";
+        for(i=10;i>0;i--){
+            System.out.println(i+"X"+2+"="+2*i);
+
+        }
+
+    }
+}
