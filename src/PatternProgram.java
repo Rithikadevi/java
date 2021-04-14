d85ba2616c4e9207cf791ffcfee1e1dca575de18
Rithikadevi <rithikadevip2@gmail.com>
2021-04-15 00:38:41 +0530

    Done pattern programs

--- src/PatternProgram.java
+++ src/PatternProgram.java
@@ -5,7 +5,7 @@ public class PatternProgram {
         int N, row, col;
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        for (row = N; row >= 1; row--) {
+        for (row = 1; row <= N; row++) {
             for (col = 1; col <= row; col++) {
                 System.out.print("*");
             }
@@ -27,7 +27,17 @@ class PatternProgramTwo {
             }
             System.out.println();
         }
-
     }
+}
 
+class PatternProgramThree {
+    public static void main(String[] args) {
+        int row, col;
+        for (row = 1; row <= 10; row++) {
+            for (col = row; col <= 10; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
 }
